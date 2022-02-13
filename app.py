0b99a28b84cfa3f18d308a704691484840a8777e
Torr496 <ilubashev@mail.ru>
2022-02-13 14:40:56 +0300

    Правки_финал

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSON AS ASCII'] = False
+app.config['JSON_AS_ASCII'] = False
 db = SQLAlchemy(app)
 
 
